609ba776c2a54acf3ba108eb279635ca45335d49
macvaar inc <macvaarinc@macvaars-macbook-pro.local>
2020-09-01 14:59:27 +0530

    NewBranch_Sample@2_Added

---
+++ SampleGitHub/ThirdViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ThirdViewController.swift
+//  SampleGitHub
+//
+//  Created by macvaar inc on 9/1/20.
+//  Copyright © 2020 macvaar inc. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       print("ThirdCommit")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
