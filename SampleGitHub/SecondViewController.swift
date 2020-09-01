d76add1cf007c83cf40e2f414446f878ec8fc5a0
macvaar inc <macvaarinc@macvaars-macbook-pro.local>
2020-09-01 14:54:29 +0530

    secondViewController is added

---
+++ SampleGitHub/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  SampleGitHub
+//
+//  Created by macvaar inc on 9/1/20.
+//  Copyright © 2020 macvaar inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("SecondCommit")
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
